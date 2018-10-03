e40b4495a2daba236f691a860b88ee831a1bccdd
Wojciech Mioduszewski <wojciech.mioduszewski@sienn.pl>
2018-10-03 15:17:39 +0200

    sandbox init

---
+++ SandboxCoreConsoleApp/SandboxCoreConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SandboxCoreConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+            Uri.IsWellFormedUriString("ftp://84.35.65.216", UriKind.Absolute));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SandboxCoreConsoleApp/SandboxCoreConsoleApp/RegexXml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SandboxCoreConsoleApp
+{
+    public static class RegexXml
+    {
+        private static readonly string SqlCommandNodeName = "SqlCommand";
+        private static readonly string Pattern = $@"<\s*{SqlCommandNodeName}\s*>\s*(?<SqlQuery>.*?)\s*<\s*/\s*{SqlCommandNodeName}\s*>";
+
+        public static void Perform()
+        {
+            string input = File.ReadAllText("XMLFile1.xml");
+            Match match = Regex.Match(input, Pattern);
+            if (match.Success)
+            {
+                var sqlQuery = match.Groups["SqlQuery"].Value;
+
+                var sqlCommandNode = match.Groups[0].Value;
+                var newSqlCommandNode = Regex.Replace(sqlCommandNode, Regex.Escape(sqlQuery),
+                    $"<![CDATA[{sqlQuery}]]>", RegexOptions.IgnorePatternWhitespace);
+
+                var newContent = Regex.Replace(input, Regex.Escape(sqlCommandNode), newSqlCommandNode);
+
+                Console.WriteLine(sqlQuery);
+                Console.WriteLine(newContent);
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
