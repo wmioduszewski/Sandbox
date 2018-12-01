a9a04748e735c5a6793dbe456c9d066142316a88
Wojciech Mioduszewski <wojciech.mioduszewski@sienn.pl>
2018-12-01 22:35:24 +0100

    advent - day 1

--- SandboxCoreConsoleApp/SandboxCoreConsoleApp/Program.cs
+++ SandboxCoreConsoleApp/SandboxCoreConsoleApp/Program.cs
@@ -1,18 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace SandboxCoreConsoleApp
 {
     class Program
     {
+        private static string filepath = @"C:\Users\WojciechMioduszewski\Desktop\input.txt";
 
         static void Main(string[] args)
         {
-            Console.WriteLine(
-            Uri.IsWellFormedUriString("ftp://84.35.65.216", UriKind.Absolute));
+            decimal sum = 0m;
+            var dict = new HashSet<decimal> { sum };
+            int rounds = 1;
+            bool res = true;
+            while (res)
+            {
+                using (var reader = new StreamReader(filepath))
+                {
+                    rounds++;
+                    while (!reader.EndOfStream)
+                    {
+                        sum += Convert.ToDecimal(reader.ReadLine());
+                        //Console.WriteLine("skladnik " + sum);
+                        res = dict.Add(sum);
+                        if(!res) break;
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(rounds);
 
             Console.ReadKey();
         }
+
+
     }
+
+
 }
